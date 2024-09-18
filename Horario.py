ce8f1cf708576f2334a3fce403f28b8fa3a9209c
Pepecola/Horario <dionisiopenalosa@iesjulioverne.es>
2024-09-18 19:49:47 +0200

    Primera version con 1 grupo

--- Horario.py
+++ Horario.py
@@ -1,7 +1,48 @@
-print('  L M X J V')
-for i in range(6):
-    print(i+1)
+from random import *
 
-materias=['Matemáticas', 'Lengua','Ciencias','Geografía e Historia'
-          ]
-profesores=['Pepe','Lola', 'Ana Victoria', 'Dionisio']
+materias=['Matemáticas', 'Lengua','Ciencias','Geografía e Historia']
+# Estructura de profesores: abrev, materia, grupo, aula, nºhoras
+profMatGru=[
+    ['Pep1','lcl','1ºESOA','A1',4],
+    ['Lol1','mat','1ºESOA','A1',4],
+    ['AnV1','ing','1ºESOA','A1',4],
+    ['Dio1','byg','1ºESOA','A1',3],
+    ['Ant1','geh','1ºESOA','A1',3]
+]
+
+# Estructura que almacena los horarios. Las filas son las horas, las columnas los días.
+horarios=[]
+for fil in range(6):
+    horarios.append([])
+    for col in range(5):
+        horarios[fil].append([])
+
+# Muestra el horario por la pantalla.
+def muestraHor():
+    for fil in range(6):
+      print()
+      for col in range(5):
+        if horarios[fil][col] != []:
+            print(horarios[fil][col],end="")
+        else:
+            print('[                   ]',end="")
+    print()
+
+# Coloca al profesor con materia pasado por parámetros en el horario.
+def coloca(prof):
+    for n in range(prof[4]):
+        hor=randint(0,5)
+        dia=randint(0,4)
+        while horarios[hor][dia] != []:
+            hor=randint(0,5)
+            dia=randint(0,4)
+        horarios[hor][dia]=[prof[0],prof[1],prof[3]]
+
+# Genera un horario.
+def genHor():
+    for prof in profMatGru:
+        coloca(prof)
+
+genHor()
+muestraHor()
+
